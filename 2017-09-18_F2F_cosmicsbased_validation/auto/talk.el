9a7ee2e005fd82dae119b4a01aa5ea098a676504
Michael Eliachevitch <michael.eliachevitch@student.kit.edu>
2017-09-18 00:04:40 +0200

    Add titles to delta t plots

--- 2017-09-18_F2F_cosmicsbased_validation/auto/talk.el
+++ 2017-09-18_F2F_cosmicsbased_validation/auto/talk.el
@@ -3,18 +3,33 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("beamer" "18pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8")))
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
    (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "beamer"
     "beamer10"
-    "templates/mytemplate"))
+    "inputenc"
+    "templates/mytemplate"
+    "templates/beamerthemekit"
+    "graphicx"
+    "microtype"
+    "listings"
+    "hyperref"
+    "multicol"
+    "amssymb")
+   (LaTeX-add-labels
+    "eq:cosmic_eff"))
  :latex)
 
